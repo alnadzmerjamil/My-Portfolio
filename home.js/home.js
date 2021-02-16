b9a6d6fae4f97b653ec1b42ab4133abacfeb0f9d
Alnadzmer jamil <alnadzmerjamil@gmail.com>
2021-02-17 02:53:46 +0800

    sana okay ka

---
+++ home.js/home.js
@@ -0,0 +1,59 @@
+// starts here
+let hamburger = document.querySelector('#hamburger-icon');
+let menuContainer = document.querySelector('#div-for-menu');
+menuContainer.style.display = 'none';
+hamburger.addEventListener('click', () => {
+  if (menuContainer.style.display === 'none') {
+    menuContainer.style.display = 'flex';
+  } else {
+    menuContainer.style.display = 'none';
+  }
+});
+// let x = document.querySelector('#div-for-menu-container');
+// x.addEventListener('mouseout', function () {
+//   alert();
+//   // menuContainer.style.display = 'none';
+// });
+
+// let home = document.querySelector('#home');
+// home.addEventListener('click', () => {
+//   alert();
+// });
+//
+// let miniContainer = document.querySelector('.mini-container');
+// miniContainer.style.width = '50%';
+// let content = document.querySelector('.content');
+// // content.style.width = '50%';
+// let about = document.querySelector('#about');
+
+// about.addEventListener('click', () => {
+//   miniContainer.style.width = '25%';
+//   miniContainer.style.transitionDelay = '0.1s';
+//   miniContainer.style.transitionDuration = '0.1s';
+
+//   // content.style.padding = '10px 0px 10px 30px';
+//   sample();
+// });
+
+// function sample() {
+//   content.style.width = '75%';
+//   content.style.transitionDelay = '0.5s';
+//   content.style.visibility = 'visible';
+// content.style.transitionDuration = '0.1s';
+// }
+//
+// let project = document.querySelector('#project');
+// project.addEventListener('click', () => {
+//   alert();
+// });
+// //
+// let contact = document.querySelector('#contact');
+// contact.addEventListener('click', () => {
+//   alert();
+// });
+
+// let upNavBar = document.querySelector('#up-nav');
+// let x = document.querySelector('#strong-name');
+// x.addEventListener('mouseover', function () {
+//   alert(upNavBar.style.top);
+// });
